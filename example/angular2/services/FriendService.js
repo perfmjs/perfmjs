0ef614b489b76b333edea2447d3d9c126a1a6b30
tony <19589425@qq.com>
2015-07-11 09:57:53 +0800

    2.1.4-rc2/ 2015-07-10 ================== * improved: 完善angular2-try例子

--- example/angular2/services/FriendService.js
+++
@@ -1,7 +0,0 @@
-var FriendService = (function () {
-    function FriendService() {
-        this.names = ["Aarav2", "Martin1", "Shannon1", "Ariana1", "Kai33"];
-    }
-    return FriendService;
-})();
-exports.FriendService = FriendService;
--- example/angular2/services/FriendService.ts
+++
@@ -1,7 +0,0 @@
-export class FriendService {
-    names: Array<String>;
-
-    constructor() {
-        this.names = ["Aarav2", "Martin1", "Shannon1", "Ariana1", "Kai33"];
-    }
-}
